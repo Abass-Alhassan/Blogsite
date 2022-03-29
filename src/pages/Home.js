9f10cf520da76c9a25ef470e3c884c18f24d3b75
Abass-Alhassan <icewrldspacely@gmail.com>
2022-03-29 08:00:59 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div>
+        <h1>Welcome to my homepage</h1>
+        <div>
+    
+    <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+             <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+             <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+    </div>
+    </div>
+)
+}
+
+export default withLayout(Home)
