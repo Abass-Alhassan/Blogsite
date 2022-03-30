6973ee1ac7d3271699f159d01babd0015823a840
Abass-Alhassan <icewrldspacely@gmail.com>
2022-03-30 06:56:03 -0700

    added route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from './pages/Home'
 import {Routes, Route} from 'react-router-dom'
+import About from'./pages/About'
+import Blog from './pages/Blog'
 
 function App() {
   return (
   <Routes>
     <Route path="/" element={<Home/>} />
+    <Route path='/about' element={<About/>}/>
+    <Route path='/blog' element={<Blog/>}/>
   </Routes>
 
 
---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function About() {
+  return (
+    <div>
+        <h1>Welcome to my about section</h1>
+        <div>
+    
+    <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+             <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+             <p>Marshall Bruce Mathers III, known professionally as
+         Eminem, is an American rapper, songwriter,
+          and record producer. Eminem is among the
+           best-selling music artists of all time,
+            with estimated worldwide sales of over
+             220 million records</p>
+    </div>
+    </div>
+)
+}
+
+export default withLayout(About)
